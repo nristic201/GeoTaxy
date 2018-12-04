44fdc3656bfc5b51fe3d701f8f56cd74efbfb3bb
Milica Martinovic <nvidol.bo@gmail.com>
2018-12-04 20:16:25 +0100

    login

---
+++ app/TestStaticQueue/app/src/main/java/com/example/milica/teststaticqueue/User.java
@@ -0,0 +1,17 @@
+package com.example.milica.teststaticqueue;
+
+/**
+ * Created by Milica on 04-Dec-18.
+ */
+
+public class User {
+
+    private String username;
+    private String password;
+
+    public User(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+}
