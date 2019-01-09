e0cfbb0e3c28c47db8b1a387d8cbcb0cda827e4e
Milica Martinovic <nvidol.bo@gmail.com>
2019-01-09 13:18:52 +0100

    login

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,40 +5,51 @@ import { amqp_url } from "../helpers";
 import { Taksista } from "./entiteti/Taksista";
 
 let app = express();
-app.use("/", (req,res) => {
-  let obj = req.body.driver;
-  createConnection().then(async conn => {
-    let t1 = await conn.getRepository(Taksista).find({
-      username: obj.username
-    });
-    if (t1[0].username === obj.username) {
-      res.json(t1);
-    } else {
-      res.json({
-        error: "wrong data"
-      });
-    }
-  });
-});
-app.listen(3000, () => {
-  // console.log("express pokrenut...");
-  // require("amqplib/callback_api").connect(
-  //   amqp_url,
-  //   (err: any, conn: any) => {
-  //     if (err) bail(err);
-  //     console.log('amqp pokrenut...')
-  //   }
-  // );
-  createConnection(db).then(()=>{
-    console.log('konekcija uspesna')
-  })
-});
-// cek cas odo da nadjen resenje ima neka forica 
-//kk
-//cekam
-//do pola 10
-//tad idem
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded());
+
 const db = require("../src/modules/ormconfig");
+
+createConnection(db).then(conn => {
+  app.post("/login", (req, res) => {
+
+    console.log("asd");
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let t1 = conn.getRepository(Taksista).find({
+      username: username
+    }).then(user => {
+
+      console.log(user);
+      if (user[0].password === password) {
+        res.json(user[0]);
+      } else {
+        res.json({
+          error: "wrong data"
+        });
+      }
+    });
+  })
+
+  app.listen(3000, () => {
+    // console.log("express pokrenut...");
+    // require("amqplib/callback_api").connect(
+    //   amqp_url,
+    //   (err: any, conn: any) => {
+    //     if (err) bail(err);
+    //     console.log('amqp pokrenut...')
+    //   }
+    // );
+
+  });
+}).catch(err => console.log(err));
+
 
 function bail(err: any) {
   console.error(err);
@@ -63,4 +74,4 @@ function replyReceivingCorrds(conn: any, msg: string) {
     ch.publish("amq.fanout", "", new Buffer(msg));
   });
 }
-const queueExist = (conn: any) => {};
+const queueExist = (conn: any) => { };
