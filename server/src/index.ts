8e140380855ca9ba4f05911495f971989035278e
Milica Martinovic <nvidol.bo@gmail.com>
2019-01-12 19:38:39 +0100

    dodata komunikacija sa bazom

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import "reflect-metadata";
 import express from "express";
-import { createConnection, getManager, getCustomRepository, Any } from "typeorm";
+import { createConnection, getManager, getCustomRepository, Any, Connection } from "typeorm";
 import { amqp_url } from "../helpers";
 import { Taksista } from "./entiteti/Taksista";
 import { Voznja } from "./entiteti/Voznja";
+import { connect } from "tls";
 
 let app = express();
 var bodyParser = require('body-parser')
@@ -16,14 +17,14 @@ app.use(express.urlencoded());
 
 const db = require("../src/modules/ormconfig");
 
-createConnection(db).then(conn => {
+const db_conn = createConnection(db).then(db_conn => {
   app.post("/login", (req, res) => {
 
     console.log(req.body);
     let username = req.body.username;
     let password = req.body.password;
 
-    let t1 = conn.getRepository(Taksista).find({
+    let t1 = db_conn.getRepository(Taksista).find({
       username: username
     }).then(user => {
 
@@ -50,8 +51,9 @@ createConnection(db).then(conn => {
         receiveCoords(conn);
         receiveRequests(conn);
         receiveResponse(conn);
-        receiveOcena(conn);
-        receiveEndRide(conn);
+        receiveStartRide(conn, db_conn);
+        receiveOcena(conn,db_conn);
+        receiveEndRide(conn,db_conn);
       }
     );
 
@@ -68,19 +70,20 @@ function receiveCoords(conn: any) {
   conn.createChannel((err: any, ch: any) => {
     if (!err == null) bail(err);
     ch.assertQueue("KoordinateTaksista");
+   
     ch.consume("KoordinateTaksista", (msg: any) => {
       if (msg) {
-        console.log(msg);
+        console.log("consumed");
         replyReceivingCorrds(conn, msg.content.toString());
       }
-    });
+    }, {noAck: true});
   });
 }
 
 function replyReceivingCorrds(conn: any, msg: string) {
   conn.createChannel((err: any, ch: any) => {
     if (!err == null) bail(err);
-    console.log(msg, "2");
+    console.log("sent");
     ch.assertExchange("amq.fanout", "fanout", { durable: true });
     ch.publish("amq.fanout", "", new Buffer(msg));
   });
@@ -94,7 +97,7 @@ function receiveRequests(conn: any) {
       if (msg) {
         replyRequests(conn, msg.content.toString());
       }
-    });
+    }, {noAck: true});
   });
 }
 
@@ -119,7 +122,7 @@ function receiveResponse(conn: any) {
         console.log(msg);
         replyResponse(conn, msg.content.toString());
       }
-    });
+    }, {noAck: true});
   });
 }
 
@@ -133,7 +136,7 @@ function replyResponse(conn: any, msg: string) {
   });
 }
 
-function receiveEndRide(conn:any) {
+function receiveEndRide(conn:any, db_conn:any) {
   conn.createChannel((err: any, ch: any) => {
 
     if (!err == null) bail(err);
@@ -141,23 +144,78 @@ function receiveEndRide(conn:any) {
     ch.assertQueue("KrajVoznjeQueue");
     ch.consume("KrajVoznjeQueue", (msg: any) => {
       if (msg) {
-        replyEndRide(conn, msg.content.toString());
+
+        let endRide = JSON.parse(msg);
+        let v;
+        db_conn.getRepository(Taksista).findOne({username: endRide.username})
+        .then(async (taksista:any) => {
+          let taksista_id = (taksista as Taksista).id;
+          db_conn.getRepository(Voznja).findOne({vozac: (taksista as Taksista), u_toku: 1})
+          .then(async (voznja:any) =>{
+              v = voznja as Voznja;
+              v.lokacija_do_lat = endRide.lat;
+              v.lokacija_do_lon = endRide.lon;
+              v.u_toku = 0;
+              db_conn.getRepository(Voznja).save(v);
+
+              replyEndRide(conn, msg.content.toString(), v);
+          }); 
+        }).catch((err:any) => console.log(err));
       }
-    });
+    }, {noAck: true});
   }); 
 }
 
-function replyEndRide(conn: any, msg: string) {
+function replyEndRide(conn: any, msg: string, v:Voznja) {
+
   conn.createChannel((err: any, ch: any) => {
+
     if (!err == null) bail(err);
+
     let endRide = JSON.parse(msg);
+    let voznja = {"id": v.id };
     let queueForResponse = endRide.queueForResponse + "1";
+
     ch.assertQueue(queueForResponse);
-    ch.sendToQueue(queueForResponse, new Buffer(msg));
+    ch.sendToQueue(queueForResponse, new Buffer(JSON.stringify(voznja)));
   });
 }
 
-function receiveOcena(conn: any) {
+function receiveStartRide(conn:any, db_conn:any) {
+
+  conn.createChannel((err: any, ch: any) => {
+
+    if (!err == null) bail(err);
+
+    ch.assertQueue("PocetakVoznjeQueue");
+    ch.consume("PocetakVoznjeQueue", (msg: any) => {
+
+      if (msg) {
+
+        let startRide = JSON.parse(msg.content.toString());
+
+        let voznja = new Voznja(startRide.lat_start, startRide.lon_start, "", "", -1, 1, new Date());
+
+          db_conn.getRepository(Taksista).findOne({username: startRide.username})
+          .then(async (res:any) => {
+
+            if(res) {
+
+                voznja.Vozac=res as Taksista;
+                db_conn.getRepository(Voznja).save(voznja);
+            }
+            else {
+
+                console.log("error upisivanja voznje")
+            }
+        })      
+      }
+    }, {noAck: true});
+  }); 
+}
+
+
+function receiveOcena(conn: any, db_conn: any) {
 
   conn.createChannel((err: any, ch: any) => {
 
@@ -166,9 +224,15 @@ function receiveOcena(conn: any) {
     ch.assertQueue("OcenaQueue");
     ch.consume("OcenaQueue", (msg: any) => {
       if (msg) {
-       //upisi u bazu
+        let ocena = JSON.parse(msg.content.toString());
+        db_conn.getRepository(Voznja).findOne({id:ocena.idVoznje})
+        .then(async (res:any) => {
+            let voznja = res as Voznja;
+            voznja.ocena = ocena.ocena;
+            db_conn.getRepository(Voznja).save(voznja);
+        });
       }
-    });
+    }, {noAck: true});
   });
 }
 const queueExist = (conn: any) => { };
