86816dc2655adfd6f0a8211c778ed35a4c57c50f
rikirafa96 <nristic201@gmail.com>
2019-02-07 17:10:24 +0100

    fix3

--- server/src/Repos/FirmaRepo.ts
+++
@@ -1,8 +0,0 @@
-import { Firma } from "../entiteti/Firma";
-import { EntityRepository, Repository } from "typeorm";
-
-
-@EntityRepository(Firma)
-export class FirmaRepo extends Repository<Firma> {
-    
-}
